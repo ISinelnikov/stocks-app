a855d7c9708434a4ff7a8a8c6e3f7c5710ad1634
isinelnikov <kingreeve@mail.ru>
2020-02-02 14:47:01 +0300

    Update token value. Base application logic.

--- Stocks/ViewController.swift
+++ Stocks/ViewController.swift
@@ -25,14 +25,14 @@ class ViewController: UIViewController {
         companyPickerView.delegate = self
 
         activityIndicator.hidesWhenStopped = true
-        
+
         requestQuoteUpdate()
     }
 
     private func requestQuote(for symbol: String) {
         print("Request quote for: \(symbol).")
-        
-        let token = "pk_4335b7641e304f6e8ef89cfe43a99cb4"
+
+        let token = "pk_33807dd047364026b2755952731a7a44"
         guard let url = URL(string: "https://cloud.iexapis.com/stable/stock/\(symbol)/quote?token=\(token)") else {
             return
         }
@@ -56,30 +56,45 @@ class ViewController: UIViewController {
 
             guard
                 let json = jsonObject as? [String: Any],
-                let companyName = json["companyName"] as? String else { return print("Json is invalid!") }
+                let companyName = json["companyName"] as? String,
+                let companySymbol = json["symbol"] as? String,
+                let price = json["latestPrice"] as? Double,
+                let priceChange = json["change"] as? Double
+            else { return print("Json is invalid!") }
 
             print(jsonObject)
-            
+
             DispatchQueue.main.async { [weak self] in
-                self?.displayStockInfo(companyName: companyName)
+                self?.displayStockInfo(companyName: companyName, companySymbol: companySymbol,
+                                       price: price, priceChange: priceChange)
             }
         } catch {
             print("Can't parse quote with error: " + error.localizedDescription)
         }
     }
-    
-    private func displayStockInfo(companyName: String) {
+
+    private func displayStockInfo(companyName: String,
+                                  companySymbol: String,
+                                  price: Double,
+                                  priceChange: Double) {
         activityIndicator.stopAnimating()
+        
         companyNameLabel.text = companyName
+        companySymbolLabel.text = companySymbol
+        priceLabel.text = "\(price)"
+        priceChangeLabel.text = "\(priceChange)"
     }
-    
+
     private func requestQuoteUpdate() {
         activityIndicator.startAnimating()
-        
+
         companyNameLabel.text = "-"
-        
+        companySymbolLabel.text = "-"
+        priceLabel.text = "-"
+        priceChangeLabel.text = "-"
+
         let selectedRow = companyPickerView.selectedRow(inComponent: 0)
-        
+
         let selectedSymbol = Array(companies.values)[selectedRow]
         requestQuote(for: selectedSymbol)
     }
@@ -99,7 +114,7 @@ extension ViewController: UIPickerViewDelegate {
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         Array(companies.keys)[row]
     }
-    
+
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         requestQuoteUpdate()
     }
