8f7bf176cce5e3b86386086fbc7bf1060f817413
isinelnikov <kingreeve@mail.ru>
2020-02-02 03:49:01 +0300

    Initial Commit

---
+++ Stocks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stocks
+//
+//  Created by Ivan Sinelnikov on 2/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
